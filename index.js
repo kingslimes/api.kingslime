61a9b24fd69d5825e8cfd694262d05f9e7dd6971
kingslime <72143717+kingslimes@users.noreply.github.com>
2023-02-19 18:01:24 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,42 @@
+const stream = require("stream");
+const server = require("express");
+const multer = require("multer")();
+const { google } = require("googleapis");
+
+const uploadRouter = server();
+uploadRouter.use( server.json({limit:"10gb"}) );
+uploadRouter.use( server.urlencoded({extended:true,limit:"10gb"}) );
+
+const uploadFile = async ( fileObject ) => {
+    const bufferStream = new stream.PassThrough();
+    bufferStream.end( fileObject.buffer );
+    const auth = new google.auth.GoogleAuth({
+        keyFile: './google.json',
+        scopes: ['https://www.googleapis.com/auth/drive']
+    })
+    const fileName = `API_IMG_${ new Date().getTime() }.${ fileObject.mimetype?.split('/')[1] }`;
+    const { data } = await google.drive({ version: 'v3', auth }).files.create({
+        media: {
+            mimeType: fileObject.mimetype,
+            body: bufferStream
+        },
+        requestBody: {
+            name: fileName,
+            parents: ['1-sQEbClcbj6xmywa5XygM3wWfGCWWF69']
+        },
+        fields: 'id'
+    });
+    return data
+};
+
+uploadRouter.post('/', multer.any(), async (req, res) => {
+    const { files } = req;
+    const result = [];
+    for ( let f = 0; f < files.length; f++ ) {
+        const file = await uploadFile( "admin", files[f] );
+        results.push( file.id )
+    }
+    res.json({ result });
+});
+
+uploadRouter.listen( process.env.TOKEN || 5555 );
